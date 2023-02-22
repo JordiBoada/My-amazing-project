5c063a3200530eb69705e9ce6e1a8732a3d8c7f1
Diego Moreno Morán <imdiegomoreno@edu25046.eduroam.gu.se>
2023-02-22 18:18:07 +0100

    cambio de colores

--- R/Jayis the best.R
+++ R/Jayis the best.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 my_amazing_data <- iris
 ggplot(my_amazing_data) + 
-  geom_point(aes(x = Sepal.Length, y = Sepal.Width, fill = Species), shape = 21) +
-  scale_fill_manual(values = c("red", "blue", "green")) +
+  geom_point(aes(x = Sepal.Length, y = Sepal.Width, fill = Species), shape = 21, size=5) +
+  scale_fill_manual(values = c("red",'yellow', "green")) +
   theme_classic()
 
 
